81767ca20b7f2fff6d264584787134e797015949
AHMED ABOUZEID MOHAMED YOUNES <ahmed.younes1376@yahoo.com>
2023-03-28 02:55:21 +0200

    -m"first merge"
    git add index.html main.js

--- main.js
+++ main.js
@@ -62,71 +62,20 @@ if (localStorage.storData != null) {
 }
 create.onclick = function () {
     let dataObject = {
-        title: title.value,
-        category: category.value,
-        price: price.value,
-        tax: tax.value,
-        ads: ads.value,
-        discount: discount.value,
-        total: total.innerHTML,
-        count: count.value,
+        theTitle: title.value,
+        theCategory: category.value,
+        thePrice: price.value,
+        theTax: tax.value,
+        theAds: ads.value,
+        theDiscout: discount.value,
+        theTotal: total.value,
     }
-    dataArray.push(dataObject)
-    localStorage.setItem('storData', JSON.stringify(dataArray));
-    clearData();
-    showAllData();
-}
-// console.log(dataArray);
-// localStorage.clear();
 
+    console.log(dataObject);
 
-// // =========================> Clear Inputs
-function clearData() {
-    title.value = '';
-    category.value = '';
-    price.value = '';
-    tax.value = '';
-    ads.value = '';
-    discount.value = '';
-    total.value = '';
-    count.value = '';
 }
 
-// // =========================>  Display The Data
-function showAllData() {
-    let dataDisplay = '';
-    for (let i = 0; i < dataArray.length; i++) {
-        dataDisplay +=
-            `
-             <tr> 
-                <td>${i}</td>
-                <td>${dataArray[i].title}</td>
-                <td>${dataArray[i].category}</td>
-                <td>${dataArray[i].price}</td>
-                <td>${dataArray[i].tax}</td>
-                <td>${dataArray[i].ads}</td>
-                <td>${dataArray[i].discount}</td>
-                <td>${dataArray[i].count}</td>
-                <td><button id="update">UPDATE</button></td>
-                <td><button id="delete"  onclick="btnDel(${i})" >DELETE</button></td>
-           </tr>
-        `
-    };
-    document.getElementById('tbody1').innerHTML = dataDisplay;
-    if (dataArray.length > 0) {
-        document.getElementById('deleteAll').style.display = 'block';
-    } else {
-        document.getElementById('deleteAll').style.display = 'none';
-    }
-}
-showAllData();
 
-// // =========================> Delete Btn By Raw
-function btnDel(i) {
-    dataArray.splice(i, 1);
-    localStorage.storData = JSON.stringify(dataArray);
-    showAllData();
-}
 
 // // =========================> Delete All Data Btn
 let bigBtnDelete = document.getElementById('deleteAll');
@@ -136,5 +85,3 @@ bigBtnDelete.onclick = function () {
 }
 
 // // =========================> Count
-
-
