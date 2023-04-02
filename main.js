0b463a9878306a6266fc0e17108dddaa2478e657
AHMED ABOUZEID MOHAMED YOUNES <ahmed.younes1376@yahoo.com>
2023-04-02 02:01:59 +0200

    finished the update code

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // =========================> Check The Ids
 
-let title = document.getElementById("title");
 let category = document.getElementById("category");
+let title = document.getElementById("title");
 let price = document.getElementById("price");
 let tax = document.getElementById("tax");
 let ads = document.getElementById("ads");
@@ -10,7 +10,11 @@ let total = document.getElementById("total");
 let create = document.getElementById("create");
 let count = document.getElementById("count");
 let search = document.getElementById("search");
-// // console.log(title, category, price, tax, ads, discount, total, count, search);
+let mood = 'creation';
+let temp;
+
+// console.log(title, category, price, tax, ads, discount, total, count, search);
+
 
 // // =========================> Create Btn
 // // let btnScrollY = document.getElementById("upBtn");
@@ -42,103 +46,146 @@ scrBtn.onclick = function () {
     })
 };
 
-// // =========================> Operation
+// =========================> Operation
+
+
 function operation() {
     if (price.value != '') {
-        let resultOperation = (+price.value + +tax.value + +ads.value) - +discount.value;
-        total.innerHTML = resultOperation;
-        total.style.color = '#44ff00';
-    } else total.style.color = '#ff0505';
+        let resuletOperation = (+price.value + +tax.value + +ads.value) - +discount.value;
+        total.innerHTML = resuletOperation;
+        total.style.color = '#00ff00';
+    } else {
+        total.innerHTML = '';
+        total.style.color = '#ff0000';
+    }
 }
 
-// // =========================> Create Data In Array/Object and LocalStorage
+
+// =========================> Create 
 let dataArray;
-if (localStorage.storData != null) {
-    dataArray = JSON.parse(localStorage.storData);
+if (localStorage.dataStore != null) {
+    dataArray = JSON.parse(localStorage.dataStore);
 } else {
     dataArray = [];
 }
+
 create.onclick = function () {
     let dataObject = {
-        title: title.value,
         category: category.value,
+        title: title.value,
         price: price.value,
         tax: tax.value,
         ads: ads.value,
+        count: count.value,
         discount: discount.value,
         total: total.innerHTML,
-        count: count.value,
-    }
-    if (dataObject.count >= 1) {
-        for (let i = 0; i < dataObject.count; i++) {
-            dataArray.push(dataObject)
+    };
+
+    localStorage.setItem('dataStore', JSON.stringify(dataArray));
+    total.style.color = '#ff0000';
+
+    //=========================>  Count
+    if (mood === 'creation') {
+        if (dataObject.count > 1) {
+            for (let i = 0; i < dataObject.count; i++) {
+                dataArray.push(dataObject);
+            }
+        } else {
+            dataArray.push(dataObject);
         }
+    } else {
+        dataArray[temp] = dataObject
+        mood = 'creation';
+        create.innerHTML = 'Create';
+        count.style.display = 'block';
     }
-
-    localStorage.setItem('storData', JSON.stringify(dataArray));
-    clearData();
-    showAllData();
+    localStorage.setItem('dataStore', JSON.stringify(dataArray));
+    clear();
+    displayData();
 }
-// console.log(dataArray);
-// localStorage.clear();
 
-// // =========================> Clear Inputs
-function clearData() {
-    title.value = '';
+// =========================> Clear Inputs
+function clear() {
     category.value = '';
+    title.value = '';
     price.value = '';
     tax.value = '';
     ads.value = '';
     discount.value = '';
-    total.value = '';
+    total.innerHTML = '';
     count.value = '';
-}
+};
 
-// // =========================>  Display The Data
-function showAllData() {
-    let dataDisplay = '';
+
+// =========================> Show Data
+
+// let tboody = document.getElementById('tbody');
+
+function displayData() {
+    let display = '';
     for (let i = 0; i < dataArray.length; i++) {
-        dataDisplay +=
+        display += `
+        <tr>
+            <td>${i}</td>
+            <td>${dataArray[i].category}</td>
+            <td>${dataArray[i].title}</td>
+            <td>${dataArray[i].price}</td>
+            <td>${dataArray[i].tax}</td>
+            <td>${dataArray[i].ads}</td>
+            <td>${dataArray[i].discount}</td>
+            <td>${dataArray[i].total}</td>
+            <td><button id="update" onclick="upDate (${i})" >update</button></td>
+            <td><button id="delete" onclick="deleteBtn(${i})">delete</button></td>
+        </tr>
             `
-             <tr> 
-                <td>${i}</td>
-                <td>${dataArray[i].title}</td>
-                <td>${dataArray[i].category}</td>
-                <td>${dataArray[i].price}</td>
-                <td>${dataArray[i].tax}</td>
-                <td>${dataArray[i].ads}</td>
-                <td>${dataArray[i].discount}</td>
-                <td>${dataArray[i].count}</td>
-                <td><button id="update">UPDATE</button></td>
-                <td><button id="delete"  onclick="btnDel(${i})" >DELETE</button></td>
-           </tr>
-        `
-    };
-    document.getElementById('tbody1').innerHTML = dataDisplay;
+    }
+    document.getElementById('tbody').innerHTML = display;
 
-    let btDall = document.getElementById("deleteAll1");
+    let DellAllBtn = document.getElementById('deleteAll1');
     if (dataArray.length > 0) {
-        btDall.innerHTML = `
-            <button> Delete All Data = ( ${dataArray.length} )</button>
-        `
-        btDall.onclick = function () {
-            dataArray.splice(0);
-            localStorage.clear();
-        }
-
+        DellAllBtn.innerHTML =
+            `
+            <button onclick='deleteAll ()' >Delete All Data : ( ${dataArray.length} )</button>
+        
+         `
     } else {
-        // document.getElementById('deleteAll').style.display = 'none';
-        btDall.innerHTML = '';
+        DellAllBtn.innerHTML = '';
     }
 }
-showAllData();
+displayData();
 
-// // =========================> Delete Btn By Raw
-function btnDel(i) {
-    dataArray.splice(i, 1);
-    localStorage.storData = JSON.stringify(dataArray);
-    showAllData();
+// =========================> Delete Btn
+
+function deleteBtn(i) {
+    dataArray.splice(i, 1).value;
+    localStorage.dataStore = JSON.stringify(dataArray);
+    displayData();
 }
 
-// // =========================> Delete All Data Btn
-// let bigBtnDelete = document.getElementById('deleteAll');
+// =========================> Delete All Btn
+
+function deleteAll() {
+    dataArray.splice(0);
+    localStorage.clear();
+    displayData();
+}
+
+// =========================> Up Date
+
+function upDate(i) {
+    // console.log(i);
+    category.value = dataArray[i].category;
+    title.value = dataArray[i].title;
+    price.value = dataArray[i].price;
+    tax.value = dataArray[i].tax;
+    ads.value = dataArray[i].ads;
+    discount.value = dataArray[i].discount;
+    // total.value = dataArray[i].total;
+    operation();
+    count.style.display = 'none';
+    create.innerHTML = "Update";
+    mood = 'upDate';
+    temp = i;
+}
+
+
